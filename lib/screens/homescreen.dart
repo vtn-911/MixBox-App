ee05b80de545f7c3ae7d4aa09eec97473568452f
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2026-08-31 11:32:31 +0700

    HomeScreen [2]: done UI

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -17,24 +17,10 @@ class _HomeScreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Icon(Icons.menu),
-        ),
-        title: const Text(
-          'MixBox',
-          style: TextStyle(
-            color: Color(0xff3525CD),
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-          ),
-        ),
-        centerTitle: true,
-      ),
+      appBar: _appbar(),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -52,73 +38,136 @@ class _HomeScreenState extends State<Homescreen> {
               const SizedBox(height: 32),
               _rTitleRecentDocs(),
               const SizedBox(height: 16),
-              SizedBox(
-                height: 230,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: recentItem.length,
-
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.only(right: 16),
-                      padding: EdgeInsets.all(16),
-                      width: 220,
-                      height: 220,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 188,
-                            height: 128,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Color(0xffEDEEEF),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadiusGeometry.circular(10),
-                              child: Image.network(
-                                recentItem[index].imageDoc,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          Text(
-                            recentItem[index].nameDoc,
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.schedule_outlined,
-                                size: 14,
-                                color: Color(0xff464555),
-                              ),
-                              Text(
-                                ' Opened ${recentItem[index].timeDoc}h',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color(0xff464555),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
+              _lvRecentDocs(),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: _bottomBar(),
+    );
+  }
+
+  SizedBox _bottomBar() {
+    return SizedBox(
+      height: 66,
+      child: BottomNavigationBar(
+        backgroundColor: Color(0xffF8F9FA),
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined, size: 20),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school_outlined, size: 20),
+            label: 'Study',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.document_scanner_outlined, size: 20),
+            label: 'Scanner',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder_outlined, size: 20),
+            label: 'Folders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined, size: 20),
+            label: 'User',
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar _appbar() {
+    return AppBar(
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Icon(Icons.menu),
+      ),
+      title: const Text(
+        'MixBox',
+        style: TextStyle(
+          color: Color(0xff3525CD),
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
+      ),
+      centerTitle: true,
+    );
+  }
+
+  SizedBox _lvRecentDocs() {
+    return SizedBox(
+      height: 230,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recentItem.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.only(right: 16),
+            padding: EdgeInsets.all(16),
+            width: 220,
+            height: 220,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff000000).withValues(alpha: 0.03),
+                  blurRadius: 2,
+                  offset: Offset(0, 1),
+                ),
+                BoxShadow(
+                  color: Color(0xff4F46E5).withValues(alpha: 0.05),
+                  blurRadius: 3,
+                  offset: Offset(0, 1),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 188,
+                  height: 128,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffEDEEEF),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                    child: Image.network(
+                      recentItem[index].imageDoc,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  recentItem[index].nameDoc,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.schedule_outlined,
+                      size: 14,
+                      color: Color(0xff464555),
+                    ),
+                    Text(
+                      ' Opened ${recentItem[index].timeDoc}h',
+                      style: TextStyle(fontSize: 14, color: Color(0xff464555)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
